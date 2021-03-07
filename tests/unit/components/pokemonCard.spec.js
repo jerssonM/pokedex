60759a67af698d4e73408d61ab84bdd323d8475d
jerssonM <jersson.m@outlook.com>
2021-03-06 22:03:51 -0500

    Increase coverage and add to .gitignore

--- tests/unit/components/pokemonCard.spec.js
+++ tests/unit/components/pokemonCard.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { mount, shallowMount } from "@vue/test-utils";
 
 import PokemonCard from "../../../src/components/PokemonCard";
@@ -20,12 +21,17 @@ describe("PokemonCard", () => {
   it("should call on click events", () => {
     const spy = jest.fn();
     const spyCloseIcon = jest.fn();
+    const spyClipboard = jest.fn();
+    global.document.execCommand = spyClipboard;
+
     const wrapper = mount(PokemonCard, {
       props: { onClick: spy, onClickCloseIcon: spyCloseIcon },
     });
     wrapper.find("button img").trigger("click");
+    wrapper.find("button").trigger("click");
     wrapper.findAll(".pokemon-card-icon-close")[0].trigger("click");
     expect(spy).toHaveBeenCalled();
     expect(spyCloseIcon).toHaveBeenCalled();
+    expect(spyClipboard).toHaveBeenCalled();
   });
 });
